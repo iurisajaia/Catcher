1ce1f1f571832ea36047026e2b4b5d2d9813cd99
sajaiaiuri <sajaiaiuri@gmail.com>
2020-08-21 18:29:32 +0400

    basic gameplay

---
+++ Assets/Scripts/Fly.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour{
+
+    public Game game;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        string tag = collision.gameObject.tag;
+        if(tag == "Ground" ^ tag == "Player"){
+            game.flyingObjectsCount++;
+
+            if(game.flyingObjectsCount % 5 == 0)
+                IncriseFallSpeed();
+            print(game.fallSpeed);
+            Destroy(gameObject);
+        }
+    }
+
+    void IncriseFallSpeed(){
+        game.fallSpeed -= 0.1f;
+    }
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour{
+
+    public GameObject FlyPrehabs;
+    public Transform Fly;
+
+    public float fallSpeed = 2f; // Object Falling Speed
+    public float flyingObjectsCount = 0; 
+
+    private float timer = 0f;
+    private float randomPosition;
+
+
+
+    void Start(){
+        
+    }
+
+    void Update(){
+        Invoke("CreateFlyingObject", 2);
+    }
+
+    void CreateFlyingObject(){
+
+        timer += Time.deltaTime;
+
+        // Spawn a new block every second
+        if (timer > fallSpeed) {
+            GameObject flyingObject = Instantiate(FlyPrehabs , Fly.position , Quaternion.identity);
+            SetMinAndMaxX();
+            flyingObject.transform.position = new Vector3(randomPosition, Fly.position.y , 0);
+            timer = 0f;
+        }
+    }
+
+    void SetMinAndMaxX() {
+		Vector3 bounds = Camera.main.ScreenToWorldPoint (new Vector3(Screen.width, Screen.height, 0));
+
+        randomPosition =  Random.Range(-bounds.x + 0.3f , bounds.x - 0.3f);
+
+	}
+}
